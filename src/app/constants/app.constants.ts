4a74385a28c7bd79d24afb1e1e5122e833c423ea
dungnguyentien1998 <dung.nguyentien1998@gmail.com>
2025-04-27 22:30:40 +0700

    Push environment file

---
+++ src/app/constants/app.constants.ts
@@ -0,0 +1,5 @@
+export class AppConstants {
+  CREATE_TRANSACTION_API = '/merchant/create-transaction';
+  SEARCH_TRANSACTION_API = '/merchant/search-transaction';
+
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiBaseUrl: 'https://gw-staging.digital.vn/'
 };
